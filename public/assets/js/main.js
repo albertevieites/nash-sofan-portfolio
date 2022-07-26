db50e758301198cef11f95ea18105c9193f3436f
albertevieites <albertevieites@gmail.com>
2022-07-26 12:10:50 +0100

    perf: :art: finish hamburger on main page

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,17 +1,7 @@
-window.onload = function () {
-  window.addEventListener("scroll", function (e) {
-    if (window.pageYOffset > 100) {
-      document.querySelector("header").classList.add("is-scrolling");
-    } else {
-      document.querySelector("header").classList.remove("is-scrolling");
-    }
-  });
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header__list");
 
-  const menu__btn = document.querySelector(".header__hamburger");
-  const mobile__menu = document.querySelector(".header__nav--mobile");
-
-  menu__btn.addEventListener("click", function () {
-    menu__btn.classList.toggle("header__hamburger--is-active");
-    mobile__menu.classList.toggle("header__hamburger--is-active");
-  });
-};
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
